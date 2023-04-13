3a36ffdab4bea2999a443d8d90944b4657314467
maxefimov <efimov.academy.top@gmail.com>
2023-04-13 21:21:16 +0300

    Добавьте файлы проекта.

---
+++ OopDateProject/OopDateProject.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+class Date
+{
+    int day;
+    int month;
+    int year;
+    int monthDays[13]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+public:
+
+    int& operator[](std::string index)
+    {
+        if (index == "day")
+            return day;
+        if (index == "month")
+            return month;
+        if (index == "year")
+            return year;
+    }
+
+};
+
+int main()
+{
+    Date d1;
+    d1["day"] = 25;
+    d1["month"] = 7;
+    d1["year"] = 2021;
+
+    int d = d1["day"];
+}
